b3e3b52ef7bb67ed437bb520409527de731e7b24
mzain4321 <zanmirza3334@gmail.com>
2025-04-04 19:08:33 +0500

    q

---
+++ app/src/main/java/com/zain/myapp1/fragmentpage.java
@@ -0,0 +1,37 @@
+package com.zain.myapp1;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+public class fragmentpage extends AppCompatActivity {
+    Button button1, button2, button3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_fragmentpage);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button1.setOnClickListener(v -> loadFragment(new fragmentOne()));
+        button2.setOnClickListener(v -> loadFragment(new FragmentTwo()));
+        button3.setOnClickListener(v -> loadFragment(new fragmentThree()));
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
+    }
+}
